bdb9d16556f6ada8b9969135c9ffc0c66e469c98
Bob <bob@elephant>
2016-12-08 14:26:54 -0900

    Caught a few bugs with respect to difference in time and something else, maybe the program checks the end of the data set for output interval rather than the top of the data set.

--- 6999filler.py
+++ 6999filler.py
@@ -6,6 +6,12 @@ import datetime
 import time
 import pytz
 
+def diag_filecheck(curfile) :
+    if curfile.find("datapro_runtime.csv") != -1 or curfile.find("_status.csv") !=-1 or curfile.find("delay.csv") !=-1 or curfile.find("site_process.csv") !=-1 :
+        print 'diagnostic file, exiting...'
+        sys.exit(1)
+    return True
+
 
 def main():
     """ Main Program Body
@@ -45,6 +51,8 @@ def main():
     ##  Open the input file                                                          ##
     ###################################################################################
     try:
+
+        a = diag_filecheck(inputfile)
         ifile = open (inputfile, 'r')
         datafile = ifile.readlines()
         ifile.close()
@@ -52,14 +60,25 @@ def main():
         print 'problem opening %s for reading (input file)' % inputfile
         sys.exit(1)
     ## grab the first value from the file to initialize things
-    curdata = datafile[4].split(',')
+    curdata = datafile[-2].split(',')
+    nextdata = datafile[-1].split(',')
     prevdate = curdata[0]
+    nextdate = nextdata[0]
     try:
         (rYr, rMo, rDay, rHr, rMin, rSec) = time.strptime(prevdate, '"%Y-%m-%d %H:%M:%S"')[0:6]
     except:
         print "pandas file, no gapping to be done"
         sys.exit(1)
+
     pastTime = datetime.datetime(rYr, rMo, rDay, rHr, rMin, rSec)
+    try:
+        (rYr, rMo, rDay, rHr, rMin, rSec) = time.strptime(nextdate, '"%Y-%m-%d %H:%M:%S"')[0:6]
+    except:
+        print "pandas file, no gapping to be done"
+        sys.exit(1)
+
+    nextTime  = datetime.datetime(rYr, rMo, rDay, rHr, rMin, rSec)
+    output_interval =  abs((nextTime - pastTime).total_seconds() )
     ## initialize output file header plus first line of data):
     output_data = datafile[:4]
     ## loop through the rest of the file looking for data gaps
@@ -68,21 +87,21 @@ def main():
         curdate = curdata[0]
         (rYr, rMo, rDay, rHr, rMin, rSec) = time.strptime(curdate, '"%Y-%m-%d %H:%M:%S"')[0:6]
         readingTime = datetime.datetime(rYr, rMo, rDay, rHr, rMin, rSec)
-        # tricky one here:  if you just do readingTime - pastTime then it just looks at the time of day.  
+        # tricky one here:  if you just do readingTime - pastTime then it just looks at the time of day.
         #    .total_seconds() is needed to return the difference in seconds properly.
         timeDiff = ( readingTime - pastTime).total_seconds()
-        if timeDiff >3600 :
+        if timeDiff >output_interval :
             # data gap found.  we need to iterate until the pastTime value
             # (filled in here) is the same as the readingTime value.
-            pastTime += datetime.timedelta(seconds=3600)
+            pastTime += datetime.timedelta(seconds=output_interval)
             timeDiff = (readingTime - pastTime).total_seconds()
             baddate = pastTime.strftime('"%Y-%m-%d %H:%M:%S"')
             baddata = baddate + ",6999\n"
             output_data.append(baddata)
             # so now need a new loop iterating on time.
-            while timeDiff > 3600 :
+            while timeDiff > output_interval :
                 ## do stuff
-                pastTime += datetime.timedelta(seconds=3600)
+                pastTime += datetime.timedelta(seconds=output_interval)
                 timeDiff = (readingTime - pastTime).total_seconds()
                 baddate = pastTime.strftime('"%Y-%m-%d %H:%M:%S"')
                 baddata = baddate + ",6999\n"
